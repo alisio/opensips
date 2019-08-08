0ea5075bec720fd99749e83232f949f36e4871d8
Antonio Alisio de Meneses Cordeiro <alisio.meneses@gmail.com>
2019-08-08 16:31:55 -0300

    Task for generic opensipsctl command

---
+++ tasks/ctl.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Puppet Task Name: ctl
+
+PATH=/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/puppetlabs/bin:/usr/local/bin/rtpproxy:/root/bin:/usr/local/bin:/opt/puppetlabs/bin/=
+opensipsctl $PT_command
